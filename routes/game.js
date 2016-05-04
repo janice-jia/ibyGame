6b7f8c49a226e6c901cd8811ce99a25f9b11612e
xiaojia <2434220573@qq.com>
2016-05-04 15:06:56 +0800

    5.04

--- routes/game.js
+++ routes/game.js
@@ -38,7 +38,7 @@ router.get('/rules', function(req, res, next) {
 
 //保存访问统计
 router.post('/log', function(req, res, next) {
-    mongodb.collection('activity_game').save(req.body,function(ree,data){
+    mongodb.collection('activity_game').save(req.body,function(err,data){
         res.send(data.ops[0]);
     });
 });
@@ -50,8 +50,9 @@ router.post('/changeNum', function(req, res, next) {
         mongodb.collection('activity_game').update(
             {_id: data._id},
             {$set: {changeN: parseInt(data.changeN) + 1}},
-            {strict: true}
-        );
+            {strict: true},function(err1,data1){
+                res.send(data1);
+            });
     });
 });
 
@@ -72,136 +73,158 @@ router.post('/share', function(req, res, next) {
     });
 });
 
+
+//抽中奖品后添加联系信息
+router.post('/addlog', function(req, res, next) {
+    mongodb.collection('activity_awd_logs').save(req.body ,function(err,data){
+        res.send(data.ops[0]);
+    });
+});
+
 router.post('/luckydraw', function(req, res, next) {
     console.info("req.body",req.body);
     var totalTimes = req.body.totalTimes;
     var activityLogId = req.body.activityLogId;
-    if(activityLogId && totalTimes){
-        var findStr = {};
-        console.info('totalTimes==============',totalTimes);
-        mongodb.collection('activity_lottery').find({actCd:"WX00002"}).toArray(
-            function(err,data){
-                if(totalTimes >= 90){
+    var url = req.protocol + '://' + req.host + req.baseUrl + req.path; //获取当前url
+    async.series({
+        /* 获取项目动态 */
+        signatureMap:function(callback){
+            signature.sign(url,function(signatureMap){
+                signatureMap.appId = wechat_cfg.appid;
+                //console.info('signatureMap=========',signatureMap);
+                callback(null, signatureMap);
+            });
+        }
+    },function(err, results){
+        if(activityLogId && totalTimes){
+            var findStr = {};
+            console.info('totalTimes==============',totalTimes);
+            mongodb.collection('activity_lottery').find({actCd:"WX00002"}).toArray(
+                function(err,data){
+                    if(totalTimes >= 90){
+                        //设置奖品
+                        setPrize(["20002", "20001", "20003", "Z9999", "Z9997", "Z9998"]);
+                        data = orderData();
+                    }else if(totalTimes >= 60 && totalTimes < 90){
+                        // >= 60 && <90s  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 谢谢参与
+                        setPrize(["20002", "20001", "20003", "20004","20005", "20006", "Z9999", "Z9997", "Z9998"]);
+                        data = orderData();
+                    }else if(totalTimes >= 30 && totalTimes < 60){
+                        // >= 30 && < 60s  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 WHS滑雪服 旅行箱 谢谢参与
+                        setPrize(["20002", "20001", "20003", "20004","20005", "20006", "20007", "20008", "Z9999", "Z9997", "Z9998"]);
+                        data = orderData();
+                    }else if(totalTimes < 30){
+                        // < 30  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 WHS滑雪服 旅行箱 iPhone SE 谢谢参与
+                        setPrize(["20002", "20001", "20003", "20004","20005", "20006", "20007", "20008", "20009", "Z9999", "Z9997", "Z9998"]);
+                        data = orderData();
+                    }
+
+
                     //设置奖品
-                    setPrize(["20002", "20001", "20003", "Z9999", "Z9997", "Z9998"]);
-                    data = orderData();
-                }else if(totalTimes >= 60 && totalTimes < 90){
-                    // >= 60 && <90s  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 谢谢参与
-                    setPrize(["20002", "20001", "20003", "20004","20005", "20006", "Z9999", "Z9997", "Z9998"]);
-                    data = orderData();
-                }else if(totalTimes >= 30 && totalTimes < 60){
-                    // >= 30 && < 60s  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 WHS滑雪服 旅行箱 谢谢参与
-                    setPrize(["20002", "20001", "20003", "20004","20005", "20006", "20007", "20008", "Z9999", "Z9997", "Z9998"]);
-                    data = orderData();
-                }else if(totalTimes < 30){
-                    // < 30  10元话费  20元话费 50元话费 100元话费 苏泊尔旅行杯 苏泊尔微压炒锅 WHS滑雪服 旅行箱 iPhone SE 谢谢参与
-                    setPrize(["20002", "20001", "20003", "20004","20005", "20006", "20007", "20008", "20009", "Z9999", "Z9997", "Z9998"]);
-                    data = orderData();
-                }
-
-
-                //设置奖品
-                //奖品名称          奖品代码        奖品位置
-                //10元话费             20002           5
-                //20元话费             20001           1
-                //50元话费             20003           8
-                //100元话费            20004           3
-                //苏泊尔旅行杯         20005           7
-                //苏泊尔微压炒锅       20006           10
-                //WHS滑雪服            20007           2
-                //旅行箱               20008           4
-                //iPhone SE            20009           0
-                //谢谢参与             Z9999           6
-                //谢谢参与             Z9997           9
-                //谢谢参与             Z9998           11
-                function setPrize(prizeList){
-                    for(var i = 0; i < data.length; i++){
-                        for(var j = 0; j < prizeList.length; j++){
-                            if(data[i].code ==  "20002" && prizeList[j] ==  "20002"){//10元话费
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 5;
-                            }else if(data[i].code == "20001" && prizeList[j] ==  "20001"){//20元话费
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 1;
-                            }else if(data[i].code == "20003" && prizeList[j] == "20003"){//50元话费
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 8;
-                            }else if(data[i].code == "20004" && prizeList[j] == "20004"){//100元话费
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 3;
-                            }else if(data[i].code == "20005" && prizeList[j] == "20005"){//苏泊尔旅行杯
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 7;
-                            }else if(data[i].code == "20006" && prizeList[j] == "20006"){//苏泊尔微压炒锅
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 10;
-                            }else if(data[i].code == "20007" && prizeList[j] == "20007"){//WHS滑雪服
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 2;
-                            }else if(data[i].code == "20008" && prizeList[j] == "20008"){//旅行箱
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 4;
-                            }else if(data[i].code == "20009" && prizeList[j] == "20009"){//iPhone SE
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 0;
-                            }else if(data[i].code == "Z9999" && prizeList[j] == "Z9999"){//谢谢参与
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 6;
-                            } else if(data[i].code == "Z9997" && prizeList[j] == "Z9997"){//谢谢参与
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 9;
-                            } else if(data[i].code == "Z9998" && prizeList[j] == "Z9998"){//谢谢参与
-                                data[i].drawStatus = true;
-                                data[i].drawStop = 11;
-                            } else {
-                                data[i].drawStatus = false;
+                    //奖品名称          奖品代码        奖品位置
+                    //10元话费             20002           5
+                    //20元话费             20001           1
+                    //50元话费             20003           8
+                    //100元话费            20004           3
+                    //苏泊尔旅行杯         20005           7
+                    //苏泊尔微压炒锅       20006           10
+                    //WHS滑雪服            20007           2
+                    //旅行箱               20008           4
+                    //iPhone SE            20009           0
+                    //谢谢参与             Z9999           6
+                    //谢谢参与             Z9997           9
+                    //谢谢参与             Z9998           11
+                    function setPrize(prizeList){
+                        for(var i = 0; i < data.length; i++){
+                            for(var j = 0; j < prizeList.length; j++){
+                                if(data[i].code ==  "20002" && prizeList[j] ==  "20002"){//10元话费
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 5;
+                                }else if(data[i].code == "20001" && prizeList[j] ==  "20001"){//20元话费
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 1;
+                                }else if(data[i].code == "20003" && prizeList[j] == "20003"){//50元话费
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 8;
+                                }else if(data[i].code == "20004" && prizeList[j] == "20004"){//100元话费
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 3;
+                                }else if(data[i].code == "20005" && prizeList[j] == "20005"){//苏泊尔旅行杯
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 7;
+                                }else if(data[i].code == "20006" && prizeList[j] == "20006"){//苏泊尔微压炒锅
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 10;
+                                }else if(data[i].code == "20007" && prizeList[j] == "20007"){//WHS滑雪服
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 2;
+                                }else if(data[i].code == "20008" && prizeList[j] == "20008"){//旅行箱
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 4;
+                                }else if(data[i].code == "20009" && prizeList[j] == "20009"){//iPhone SE
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 0;
+                                }else if(data[i].code == "Z9999" && prizeList[j] == "Z9999"){//谢谢参与
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 6;
+                                } else if(data[i].code == "Z9997" && prizeList[j] == "Z9997"){//谢谢参与
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 9;
+                                } else if(data[i].code == "Z9998" && prizeList[j] == "Z9998"){//谢谢参与
+                                    data[i].drawStatus = true;
+                                    data[i].drawStop = 11;
+                                } else {
+                                    data[i].drawStatus = false;
+                                }
                             }
                         }
                     }
-                }
 
-                function orderData(){
-                    var aData = [];
-                    for (var i = 0; i < data.length; i++){
-                        if(data[i].code == "20009"){
-                            aData[0] = data[i];
-                        }else if(data[i].code == "20001"){
-                            aData[1] = data[i];
-                        }else if(data[i].code == "20007"){
-                            aData[2] = data[i];
-                        }else if(data[i].code == "20004"){
-                            aData[3] = data[i];
-                        }else if(data[i].code == "20008"){
-                            aData[4] = data[i];
-                        }else if(data[i].code == "20002"){
-                            aData[5] = data[i];
-                        }else if(data[i].code == "Z9999"){
-                            aData[6] = data[i];
-                        }else if(data[i].code == "20005"){
-                            aData[7] = data[i];
-                        }else if(data[i].code == "20003"){
-                            aData[8] = data[i];
-                        }else if(data[i].code == "Z9997"){
-                            aData[9] = data[i];
-                        }else if(data[i].code == "20006"){
-                            aData[10] = data[i];
-                        }else if(data[i].code == "Z9998"){
-                            aData[11] = data[i];
+                    function orderData(){
+                        var aData = [];
+                        for (var i = 0; i < data.length; i++){
+                            if(data[i].code == "20009"){
+                                aData[0] = data[i];
+                            }else if(data[i].code == "20001"){
+                                aData[1] = data[i];
+                            }else if(data[i].code == "20007"){
+                                aData[2] = data[i];
+                            }else if(data[i].code == "20004"){
+                                aData[3] = data[i];
+                            }else if(data[i].code == "20008"){
+                                aData[4] = data[i];
+                            }else if(data[i].code == "20002"){
+                                aData[5] = data[i];
+                            }else if(data[i].code == "Z9999"){
+                                aData[6] = data[i];
+                            }else if(data[i].code == "20005"){
+                                aData[7] = data[i];
+                            }else if(data[i].code == "20003"){
+                                aData[8] = data[i];
+                            }else if(data[i].code == "Z9997"){
+                                aData[9] = data[i];
+                            }else if(data[i].code == "20006"){
+                                aData[10] = data[i];
+                            }else if(data[i].code == "Z9998"){
+                                aData[11] = data[i];
+                            }
                         }
+                        return aData;
                     }
-                    return aData;
+                    res.render('luckydraw', {
+                        title: '抽奖',
+                        lotteryList:data,       //奖品list
+                        totalTimes:totalTimes,   //拼图总时间
+                        signatureMap:results.signatureMap,//微信签名信息
+                        activityLogId:activityLogId   //当前用户拼图_id
+                    });
                 }
-                res.render('luckydraw', {
-                    title: '抽奖',
-                    lotteryList:data,       //奖品list
-                    totalTimes:totalTimes,   //拼图总时间
-                    activityLogId:activityLogId   //当前用户拼图_id
-                });
-            }
-        );
-    }else{
-        res.send("没有获取到拼图时间哇！！！");
-    }
+            );
+        }else{
+            res.send("没有获取到拼图时间哇！！！");
+        }
+    })
+
 
 });
 
