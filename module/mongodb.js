9e6f77404299149079e14c16daf06c662b2e9854
xiaojia <2434220573@qq.com>
2016-05-05 10:07:39 +0800

    修改mogo配置

--- module/mongodb.js
+++ module/mongodb.js
@@ -1,7 +1,12 @@
-//mongoskin链接
-//var db = require('mongoskin').db('mongodb://superadmin:123456@192.168.1.6:10000/bykj_act');
-var db = require('mongoskin').db('mongodb://xiaojia:qwe123@localhost:27017/mongoskin');
-db.bind('mongoskin');
+//mongo本地配置
+//var db = require('mongoskin').db('mongodb://xiaojia:qwe123@localhost:27017/mongoskin');
+//db.bind('mongoskin');
+
+
+//mongo外网配置
+var db = require('mongoskin').db('mongodb://root:123456@mongo.dbserver:27017,mongo.dbserver2:27017/bykj_act?replicaSet=mas');
+db.bind('bykj_act');
+
 
 
 module.exports = db;
