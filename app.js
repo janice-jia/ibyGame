2488bc4b07046c785ddf100cdf60cacbaf2e97c5
xiaojia <2434220573@qq.com>
2016-05-05 11:24:21 +0800

    修改端口

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var game = require('./routes/game');
 
-var app = express();
+//更改默认端口
+process.env.PORT = 3001
 
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
